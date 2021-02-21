0c3b9832eb92e2591deea95ca7a41dc1d3fe55f4
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2021-02-21 22:02:48 +0100

    update IP address

--- src/api/helper.js
+++ src/api/helper.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://192.168.0.156:3000/";
+export const BASE_URL = ""; // declare your host here e.g ''
 
 export async function get(endpoint, params) {
   return new Promise((resolve) => {
